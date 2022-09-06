273c29d98a34dea8d8ecda24517eb530698f6c9a
denisperaltac <denisperaltac@gmail.com>
2022-09-06 16:09:09 -0300

    Pelu

--- SH-front/src/Components/Login/PasswordD.jsx
+++ SH-front/src/Components/Login/PasswordD.jsx
@@ -1,39 +1,59 @@
-import React from "react";
-import "./login.css"
-import Vector1 from "../../assets/Vector1.svg"
-import Vector2 from "../../assets/Vector2.svg"
-import LogoLogin from "../../assets/LogoLogin.svg"
-import Continuar from "../../assets/Continuar.svg"
-import VectorD1 from "./VectorD1.svg"
-import VectorD2 from "./VectorD2.svg"
-import VectorD3 from "./VectorD3.svg"
-import VectorD4 from "./VectorD4.svg"
-import VectorD5 from "./VectorD5.svg"
-const PasswordDComponent = ({
-    password,
-setPassword,
-onSubmit
-}) => {
+import React, { useEffect, useState } from "react";
+import "./login.css";
 
-    return(
-        <div className="DContainerDiv">
-            
-        <img src={LogoLogin} className="DLogoLogin"/>
-   
-        <p className="DBienvenide">BIENVENIDA SABINA</p>
-        <p className="DDNI">PASSWORD</p>
-       
-        <input type={"password"} value={password} className="DInput" onChange={(e)=>{setPassword(e.target.value)}}></input>
-        <img src={Continuar} className="Dbotonn" onClick={()=>{onSubmit()}}/>
-       
-        <p className="Dfooter">Sabina Hairabedian Consultorios Bv. San Juan 670 - Piso 6 </p>
-        <img src={VectorD1} alt="vector" className="VectorD1" />
-           <img src={VectorD2} alt="vector" className="VectorD2" />
-            <img src={VectorD3} alt="vector" className="VectorD3" />
-             <img src={VectorD4} alt="vector" className="VectorD4" />
-            <img src={VectorD5} alt="vector" className="VectorD5" />
-        </div>
-    )
-}
+import LogoLogin from "../../assets/LogoLogin.svg";
+import Continuar from "../../assets/Continuar.svg";
+import PopUpContra from "../../assets/PopUpContra.svg";
 
-export default PasswordDComponent
+const PasswordDComponent = ({ password, setPassword, onSubmit, isError }) => {
+  const [error, setError] = useState("offPop");
+  const [errorInput, setErrorInput] = useState("Input");
+
+  useEffect(() => {
+    if (isError === true) {
+      setError("onPop");
+    }
+  }, [isError]);
+
+  useEffect(() => {
+    if (isError === true) {
+      setErrorInput("InputOff");
+    }
+  }, [isError]);
+  return (
+    <div className="DContainerDiv">
+      <img src={LogoLogin} className="DLogoLogin" alt="Logo" />
+      <img
+        src={PopUpContra}
+        alt="popUp"
+        className={isError === false ? "offPop" : "onPop"}
+      />
+
+      <p className="DBienvenide">BIENVENIDA SABINA</p>
+      <p className="DDNI">PASSWORD</p>
+
+      <input
+        type={"password"}
+        value={password}
+        className={isError === false ? "Input" : "InputOff"}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      ></input>
+      <img
+        src={Continuar}
+        alt="btn"
+        className="Dbotonn"
+        onClick={() => {
+          onSubmit();
+        }}
+      />
+
+      <p className="Dfooter">
+        Sabina Hairabedian Consultorios Bv. San Juan 670 - Piso 6{" "}
+      </p>
+    </div>
+  );
+};
+
+export default PasswordDComponent;
--- SH-front/src/Components/Login/loginD.jsx
+++ SH-front/src/Components/Login/loginD.jsx
@@ -1,64 +1,64 @@
-import React from "react";
-import "./login.css"
+import React, { useEffect, useState } from "react";
+import "./login.css";
 
-import LogoLogin from "../../assets/LogoLogin.svg"
-import Continuar from "../../assets/Continuar.svg"
-import LogoStart from "../../assets/LogoStart.svg"
-import VectorD1 from "./VectorD1.svg"
-import VectorD2 from "./VectorD2.svg"
-import VectorD3 from "./VectorD3.svg"
-import VectorD4 from "./VectorD4.svg"
-import VectorD5 from "./VectorD5.svg"
-  
+import LogoLogin from "../../assets/LogoLogin.svg";
+import LogoStartNew from "../../assets/LogoStartNew.svg";
+import Continuar from "../../assets/Continuar.svg";
+import PopUpDNI from "../../assets/PopUpDNI.svg";
 
-const LoginDComponent = ({
-    dni,
-    setDni,
-    onSubmit
-}) => {
+const LoginDComponent = ({ dni, setDni, onSubmit, isError }) => {
+  const [error, setError] = useState("offPop");
+  const [errorInput, setErrorInput] = useState("Input");
 
-    return(
-        <div className="Dcontainer">
-            <div className="Dloader"></div>
-        {/* <div className="Dgrow"></div>
+  useEffect(() => {
+    if (isError === true) {
+      setError("onPop");
+    }
+  }, [isError]);
+
+  useEffect(() => {
+    if (isError === true) {
+      setErrorInput("InputOff");
+    }
+  }, [isError]);
+  return (
+    <div className="Dcontainer">
+      <img src={PopUpDNI} alt="popUp" className={error} />
+      <div className="grow"></div>
+      <div className="Dloader"></div>
+      {/* <div className="Dgrow"></div>
         <img src={LogoStart} className="Dlogo" alt="logo"></img> */}
-        <div className="Dwhi">
-          <div className="DContainerDiv">
-           
-            <img src={LogoLogin} alt="vector" className="DLogoLogin" />
-           
-            <p className="DBienvenide">BIENVENIDA/O</p>
-            <p className="DDNI">D.N.I</p>
-  
-            <input
-              value={dni}
-              className="DInput"
-              onChange={(e) => {
-                setDni(e.target.value);
-              }}
-            ></input>
-            <img
-              src={Continuar}
-              alt="vector"
-              className="Dbotonn"
-              onClick={onSubmit}
-            />
-  
-            <p className="Dfooter">
-              Sabina Hairabedian Consultorios Bv. San Juan 670 - Piso 6{" "}
-            </p>
-          </div>
-        </div>
-        {/* <div className="containerStart">
-          <img src={LogoStart} className="logo"></img> 
-          <div className="grow"></div> */}
-                      <img src={VectorD1} alt="vector" className="VectorD1" />
-           <img src={VectorD2} alt="vector" className="VectorD2" />
-            <img src={VectorD3} alt="vector" className="VectorD3" />
-             <img src={VectorD4} alt="vector" className="VectorD4" />
-            <img src={VectorD5} alt="vector" className="VectorD5" />
+      <div className="containerGrow">
+        <img src={LogoStartNew} className="logo2D" alt="logo"></img>
       </div>
-    )
-}
+      <div className="Dwhi">
+        <div className="DContainerDiv">
+          <img src={LogoLogin} alt="vector" className="DLogoLogin" />
 
-export default LoginDComponent
+          <p className="DBienvenide">BIENVENIDA/O</p>
+          <p className="DDNI">D.N.I</p>
+
+          <input
+            value={dni}
+            className={errorInput}
+            onChange={(e) => {
+              setDni(e.target.value);
+            }}
+          ></input>
+          <img
+            src={Continuar}
+            alt="vector"
+            className="Dbotonn"
+            onClick={onSubmit}
+          />
+
+          <p className="Dfooter">
+            Sabina Hairabedian Consultorios Bv. San Juan 670 - Piso 6{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginDComponent;
